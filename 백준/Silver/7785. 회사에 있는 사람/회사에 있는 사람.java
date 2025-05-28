a8dd5c35638a00d0c0f030a5759bd4967838e5d5
RACHEL-2929 <83777032+RACHEL-2929@users.noreply.github.com>
2025-05-28 21:45:24 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 1232 ms, Memory: 55756 KB -BaekjoonHub

--- 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.java
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.java
@@ -5,26 +5,31 @@ import java.io.*;
 // The main method must be in a class named "Main".
 class Main {
 
-    
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    
+
         int num = Integer.parseInt(br.readLine());
 
+        Set<String> names = new HashSet<>();
         StringTokenizer st;
-        Set<String> set = new HashSet<>();
-        String name = "";
-        String present="";
-        for(int i = 0; i< num; i++){
-            st = new StringTokenizer(br.readLine());
-            name = st.nextToken();
-            present = st.nextToken();
 
-            if(present.equals("enter")) set.add(name);
-            else set.remove(name);
+        for(int i = 0; i<num; i++){
+            st = new StringTokenizer(br.readLine());
+            String name = st.nextToken();
+            String EoL = st.nextToken();
+            if(EoL.equals("enter")) names.add(name);
+            else names.remove(name);
         }
 
-        set.stream().sorted(Comparator.reverseOrder()).forEach(System.out::println);
+        List<String> list = new ArrayList<>(names);
+        Collections.sort(
+            list,(a,b)-> {return b.compareTo(a);}
+        );
+        for(String n: list){
+            System.out.println(n);
+        }
+        
+        
     }
-
 }
